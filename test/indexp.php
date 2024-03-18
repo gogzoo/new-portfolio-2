6166991ad32a888616615fc602aff97d690c7370
kuku <purue3131@naver.com>
2024-03-18 20:28:21 +0900

    first commit

---
+++ test/indexp.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>곽지성</h2>
+    <ul>
+        <li><a href="./1_megastudy/index.html">메가스터디</a></li>
+        <li><a href="./2_tsugunohi/index.html">이은날</a></li>
+        <li><a href="#">제작중...</a></li>
+    </ul>
+</body>
+</html>
